4632ff334950dc807482aba5705bdd7e572d7375
Sagar Dhuri <99194347+sagarDhuri999@users.noreply.github.com>
2022-06-13 20:51:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Redux from "./components/Redux";
+// import UserCard from "./components/UserCard";
+
+
+function App() {
+
+
+  return (
+    <div>
+      {/* <UserCard /> */}
+      <Redux />
+    </div>
+  );
+}
+
+export default App;
